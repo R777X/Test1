d7dfd1bb5d29cb899972e70bbc1f40630990baa5
R777X <kapik58@wp.pl>
2019-09-27 21:41:02 +0200

    Issue1 solved2

--- Test1/Program.cs
+++ Test1/Program.cs
@@ -11,7 +11,7 @@ namespace Test1
         static void Main(string[] args)
         {
             Console.WriteLine("DASJASDJ");
-            Console.WriteLine("DSFSDFFSD");
+            Console.WriteLine("DSFSDFFSasdasdD");
         }
     }
 }
